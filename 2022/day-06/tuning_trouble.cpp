8980cf5ecde103edf43cf59a25bdfb531e51c504
Osman Said <57035358+Umid0vic@users.noreply.github.com>
2022-12-17 00:31:13 +0100

    Solved the second puzzle of day 6

--- 2022/day-06/tuning_trouble.cpp
+++ 2022/day-06/tuning_trouble.cpp
@@ -6,28 +6,38 @@
 using namespace std;
 
 // Returns the position of the first start-of-packet marker in the given buffer
-int findMarker(std::string buffer) {
-  string lastFour;
-  for (int i = 0; i < buffer.length(); i++) {
-    lastFour += buffer[i];
-    if (lastFour.length() > 4) {
-      lastFour.erase(0, 1);
-    }
-    // If lastFour has 4 characters and they are all different, return the current position
-    if (lastFour.length() == 4 && set<char>(lastFour.begin(), lastFour.end()).size() == 4) {
-      return i+1;
+int findStartOfPacket(string buffer) {
+  for (int i = 0; i < buffer.length() - 3; i++) {
+    string substr = buffer.substr(i, 4);
+    // If the last 4 characters are all different, return the position of the fourth character
+    if (set<char>(substr.begin(), substr.end()).size() == 4) {
+      return i+4;
     }
   }
   // If the loop completes and no marker was found
   return -1;
 }
 
+// Returns the position of the first start-of-message marker in the given buffer
+int findStartOfMessage(string buffer) {
+  for (int i = 0; i < buffer.length() - 13; i++) {
+    string substr = buffer.substr(i, 14);
+    // If the last 14 characters are all different, return the position of the fourteenth character
+    if (set<char>(substr.begin(), substr.end()).size() == 14) {
+      return i+14;
+    }
+  }
+  return -1;
+}
+
 int main() {
   string line;
   ifstream file("datastream.txt");
-
   getline(file, line);
-  cout << findMarker(line) << std::endl;
+  // Get the start-of-packet
+  cout << "The start-of-packet marker is detected at position : " << findStartOfPacket(line) << std::endl;
+  // Get the start-of-message
+  cout << "The start-of-message marker is detected at position : " << findStartOfMessage(line) << std::endl;
   
   return 0;
 }
