a9cf4ce32ab07a81f2e3160b8041f596f5702acf
Osman Said <57035358+Umid0vic@users.noreply.github.com>
2022-12-26 20:49:24 +0100

    puzzle 1 of day 7 is solved!

--- 2022/day-07/file_system.cpp
+++ 2022/day-07/file_system.cpp
@@ -1,140 +1,137 @@
+
 #include <iostream>
+#include <string>
 #include <unordered_map>
 #include <vector>
 #include <fstream>
-#include <string>
 
 using namespace std;
 
-// A struct that represents a directory or a file in the file system
+// File struct to represent a file or directory
 struct File {
     string name;
     int size;
-    bool isDirectory;
-    vector<File> children;
+    unordered_map<string, File> children;  // Only used for directories
+    File* parent;
 };
 
-// A function to parse a line of input and create a File object
-File parseInputLine(string line) {
+// Function to create a new file or directory based on the given line of input
+// The file or directory will be added to the current directory specified by pCurrentDir
+void createFile(File *pCurrentDir, string line) {
     File file;
-    if (line.substr(0,3) == "dir") {
-        // The line represents a directory
-        file.isDirectory = true;
-        int spaceIndex = line.find(' ');
-        file.name = line.substr(spaceIndex + 1);
-        cout << "log: parsed a dir with name : " << file.name  << endl;
+    if (line.substr(0, 4) == "dir ") {
+      // Create a new directory
+        string dirName = line.substr(4);
+        file.name = dirName;
+        file.size = -1;
+        file.parent = pCurrentDir;
+        pCurrentDir->children[dirName] = file;
     } else {
-        // The line represents a file
-        file.isDirectory = false;
-        int spaceIndex = line.find(' ');
-        file.size = stoi(line.substr(0, spaceIndex));
-        file.name = line.substr(spaceIndex + 1);
-        cout << "log: parsed a file with name : " << file.name << " and size : " << file.size << endl;
+        // Create a new file
+        int size = stoi(line.substr(0, line.find(' ')));
+        string fileName = line.substr(line.find(' ') + 1);
+        file.name = fileName;
+        file.size = size;
+        file.parent = pCurrentDir;
+        pCurrentDir->children[fileName] = file;
     }
-    return file;
 }
 
-// Recursive function to calculate the total size of a directory
-int calculate_directory_size(File& directory) {
+// Recursive function to calculate the total size of a directory in bytes
+// The size of a directory is the sum of the sizes of all its files and subdirectories
+int calculateDirectorySize(File& directory) {
+    int totalSizeOfDir = 0;
     int size = 0;
-    for ( File& child : directory.children) {
-        if (child.isDirectory) {
-            // Recursively calculate the size of this child directory
-            size += calculate_directory_size(child);
+    for (auto& [name, child] : directory.children) {
+        if (child.size == -1) {
+        // Recursively calculate the size of this child directory
+        size += calculateDirectorySize(child);
         } else {
-            // This child is a file, so add its size to the total
-            size += child.size;
+        // This child is a file, so add its size to the total
+        size += child.size;
         }
     }
     return size;
 }
 
-
-
-int main() {
-    
-    vector<string> input;
-    string line;
-    ifstream datastream("file.txt");
-
-    while (getline(datastream, line)) {
-        input.push_back(line);
+// Recursive function to traverse the file system and sum up the sizes
+// of all the directories that have at most 100000 bytes in total
+int sumDirectorySizes(File& dir, int& totalSize) {
+    int dirSize = calculateDirectorySize(dir);
+    if (dirSize <= 100000) {
+        totalSize += dirSize;
     }
-    // The keys of the map are strings representing the names of directories, 
-    // and the values are pointers to the corresponding File objects representing those directories. 
-    unordered_map<string, File*> directories;
-    // This vector is used to store the entire file and directory tree, 
-    // with the root directory being the first element in the vector.
-    vector<File> files;
-    File* currentDirectory = &files.emplace_back();
-    currentDirectory->isDirectory = true;
-    // A root directory
-    currentDirectory->name = "/";
-    directories["/"] = currentDirectory;
-
-    for (const string& line: input) {
-        
-        if (line[0] == '$') {
-            // Commands
-            if (line == "$ cd /") {
-                // Change the current directory to the root directory
-                currentDirectory = directories["/"];
-                cout << "log: changing to root dir " << line << endl;
-                cout << "log: curentDir is " << currentDirectory->name << endl;
-            } else if (line.substr(0, 4 ) == "$ cd") {
-
-                if (line == "$ cd ..") {
-                    cout << "log: changing to parent " << line << endl;
-                    currentDirectory = directories["/"];
-                    for (auto file : files) {
-                        if (file.isDirectory) {
-                            for (auto child : file.children) {
-                                if (&child == currentDirectory) {
-                                    // The current directory is a child of this directory, so this is the parent
-                                    currentDirectory = &file;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    
-                    cout << "log: curentDir is " << currentDirectory->name << endl;
-                } else {
-                    // Change the current directory to a subdirectory
-                    
-                    string subdirectoryName = line.substr(5);
-                    File* subdirectory = directories[subdirectoryName];
-                    if (subdirectory == nullptr) {
-                        // This subdirectory does not exist yet, so create it
-                        cout << "log: creating subdir " << line << endl;
-                        subdirectory = &files.emplace_back();
-                        subdirectory->isDirectory = true;
-                        subdirectory->name = subdirectoryName;
-                        currentDirectory->children.push_back(*subdirectory);
-                    }
-                    cout << "log: changing to subdir " << line << endl;
-                    currentDirectory = subdirectory;
-                    cout << "log: curentDir is " << currentDirectory->name << endl;
-                }
-
-            } else if (line == "$ ls") {
-                // List the contents of the current directory
-                cout << "log: ls command " << line << endl;
-            }
-        } else {
-            // This line is a file or directory, so add it to the current directory
-            File file = parseInputLine(line);
-            currentDirectory->children.push_back(file);
-            if (file.isDirectory) {
-                directories[file.name] = &currentDirectory->children.back();
-                cout << "log: added " << line << endl;
-            }
+    // Traverse the children of the current directory
+    for (auto& [name, child] : dir.children) {
+        // If the child is a directory, recursively sum its sizes
+        if (child.size == -1) {
+        sumDirectorySizes(child, totalSize);
         }
     }
+    return totalSize;
+}
 
-    vector<File> small_directories;
-    // TODO: Calculate total size of small directories.
+// Function to print the file structure of a directory and its children
+void printFileStructure(const File& file, int level) {
+    // Print the file name and type
+    for (int i = 0; i < level; i++) {
+        cout << "  ";
+    }
+    cout << "- " << file.name;
+    if (file.size == -1) {
+        cout << " (dir)";
+    } else {
+        cout << " (file, size=" << file.size << ")";
+    }
+    cout << endl;
+    // Recursively print the children
+    for (const auto& [name, child] : file.children) {
+        printFileStructure(child, level + 1);
+    }
+}
 
+int main() {
+
+  // Create the root directory
+  File root;
+  root.name = "/";
+  root.size = -1;
+  root.parent = nullptr;
+  File* pCurrentDir = &root;
+  
+  ifstream input("input.txt");
+  string line;
+
+    while (getline(input, line)) {
+        // Change directory
+        if (line.substr(0, 4) == "$ cd") {
+        string command = line.substr(5);
+
+        if (command == "/") {
+            // Set the current directory to the root directory
+            pCurrentDir = &root;
+        } else if (command == "..") {
+            /// Set the current directory to the parent directory if it exists
+            if (pCurrentDir->parent) pCurrentDir = pCurrentDir->parent;
+            else cout << "Error: Already at root directory" << endl;
+        } else {
+            // Set the current directory to the subdirectory if it exists
+            string subdir_name = line.substr(5);
+            if (pCurrentDir->children.count(subdir_name)) {
+            pCurrentDir = &pCurrentDir->children[subdir_name];
+            } else {
+            cout << "Error: No such directory" << endl;
+            }
+        }
+        } else if (line != "$ ls") {
+            createFile(pCurrentDir, line);
+        }
+    }
+    // Print file structure
+    printFileStructure(root, 0);
+
+    int sum = sumDirectorySizes(root, sum);
+    cout << "Sum of sizes of all directories with at least 100000 bytes: " << sum << endl;
 
     return 0;
 }
